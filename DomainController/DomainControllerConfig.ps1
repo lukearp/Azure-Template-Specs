30d466cd4f9dc0d82a73687324f859ec85bb10ed
Luke Arp <ruarp@microsoft.com>
2020-10-15 15:05:21 -0400

    Added som Default ARM Templates to made Specs

---
+++ DomainController/DomainControllerConfig.ps1
@@ -0,0 +1,59 @@
+configuration DomainControllerConfig
+{
+    param
+    (    
+        [Parameter(mandatory = $true)]
+        [string]$username,
+        [Parameter(Mandatory = $true)]
+        [securestring]$password,
+        [Parameter(mandatory = $true)]
+        [string]$domain,
+        [Parameter(mandatory = $false)]
+        [string]$site = "Default-First-Site-Name"
+    )
+    
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+    $unsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    node localhost
+    {
+        WindowsFeature ADDSInstall {
+            Ensure               = 'Present'
+            Name                 = 'AD-Domain-Services'
+            IncludeAllSubFeature = $true
+        }
+    
+        Script SetupDisk {
+            DependsOn = "[WindowsFeature]ADDSInstall"
+            GetScript  = {
+                return @{'Result' = '' }
+            }
+            SetScript  = {
+                $disk = Get-Disk -Number 2
+                New-Volume -Disk $disk -FileSystem NTFS -DriveLetter F -FriendlyName "AD-Files"
+            }
+            TestScript = {
+                return ((Get-Volume -DriveLetter F -ErrorAction SilentlyContinue) -ne $null)
+            }
+        }
+    
+        Script DCPromo {
+            GetScript  = {
+                return @{'Result' = '' }
+            }
+            SetScript  = {
+                
+                $securepassword = ConvertTo-SecureString -String $using:unsecurePassword -AsPlainText -Force
+                $domainCredential = New-Object System.Management.Automation.PSCredential ($using:username, $securepassword)
+                New-Item -Path "F:\NTDS" -ItemType Directory;
+                New-Item -Path "F:\SYSVOL" -ItemType Directory;    
+                
+                Install-ADDSDomainController -SkipPreChecks -DomainName $using:domain -SafeModeAdministratorPassword $securepassword -SiteName $using:site -Credential $domainCredential -DatabasePath "F:\NTDS" -SysvolPath "F:\SYSVOL" -Confirm:$false -Force;
+            }
+            TestScript = {
+                return ((Get-Item -Path F:\SYSVOL -ErrorAction SilentlyContinue) -ne $null)
+            }
+        } 
+    }
+}
+DomainControllerConfig
